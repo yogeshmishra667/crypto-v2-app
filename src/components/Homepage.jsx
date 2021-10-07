da1b27c23c8c875d7d2f930f55e337280fa6551d
yogesh mishra <yogeshmishra667@gmail.com>
2021-10-07 21:52:58 +0530

    complete basic design like layout and footer

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
-
+import millify from 'millify'
+import { Typography, Statistic, Row, Col } from 'antd'
+import { Link } from 'react-router-dom'
+const { Title } = Typography
 const Homepage = () => {
-    return <div>home</div>
+    return (
+        <>
+            <Title level={2} className="heading">
+                Global stats
+            </Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title="total cryptocurrency" value={5} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="total exchanges" value={5} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="total market cap" value={5} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="total 24h volume" value={5} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="total market" value={5} />
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default Homepage
