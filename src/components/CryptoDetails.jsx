d038a4702c5863041277a6e2551963b0ae369094
yogesh mishra <yogeshmishra667@gmail.com>
2021-10-21 00:51:44 +0530

    added details design

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,6 +15,7 @@ import {
   ThunderboltOutlined,
 } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { SemipolarLoading } from 'react-loadingg';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -24,8 +25,11 @@ const CryptoDetails = () => {
   //console.log(coinId);
   const [timePeroid, settimePeroid] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-  //console.log(data);
-  const cryptoDetails = data?.data?.coin;
+
+  let cryptoDetails = data?.data?.coin;
+  //console.log(cryptoDetails.name);
+
+  if (isFetching) return <SemipolarLoading color="#0071bd" />;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
@@ -95,7 +99,75 @@ const CryptoDetails = () => {
     },
   ];
 
-  return <div>crypto details : {coinId} </div>;
+  return (
+    <Col className="coin-detail-container">
+      <Col className="coin-heading-container">
+        <Title level={2} className="coin-name">
+          {data?.data?.coin.name} ({data?.data?.coin.slug}) Price
+        </Title>
+        <p>
+          {cryptoDetails.name} live price in US Dollar (USD). View
+          value statistics, market cap and supply.
+        </p>
+      </Col>
+      <Select
+        defaultValue="7d"
+        className="select-timeperiod"
+        placeholder="Select Timeperiod"
+        onChange={(value) => settimePeroid(value)}
+      >
+        {time.map((date) => (
+          <Option key={date}>{date}</Option>
+        ))}
+      </Select>
+
+      <Col className="stats-container">
+        <Col className="coin-value-statistics">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              {cryptoDetails.name} Value Statistics
+            </Title>
+            <p>
+              An overview showing the statistics of{' '}
+              {cryptoDetails.name}, such as the base and quote
+              currency, the rank, and trading volume.
+            </p>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+        </Col>
+
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              Other Stats Info
+            </Title>
+            <p>
+              An overview showing the statistics of{' '}
+              {cryptoDetails.name}, such as the base and quote
+              currency, the rank, and trading volume.
+            </p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
+    </Col>
+  );
 };
 
 export default CryptoDetails;
